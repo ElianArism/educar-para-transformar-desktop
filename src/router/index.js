9ab9d9a34200e20a26b0e504af3cb08c0680fadb
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-10-10 18:57:06 -0300

    logo

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "home",
     component: HomeView,
   },
   {
-    path: "/login",
+    path: "/",
     name: "login",
     component: () =>
       import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
