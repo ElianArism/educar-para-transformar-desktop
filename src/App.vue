b1d862c60d972c788bc35face2222d6b8f5b4b1d
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-10-10 17:44:04 -0300

    nav y login

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
+<script setup lang="js">
+// import { RouterLink } from "vue-router";
+import Nav from "@/components/Nav.vue";
+</script>
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    |
+    <router-link to="/login">Login</router-link>
     <button class="btn btn-primary"></button>
-  </nav>
-  <router-view />
+  </nav> -->
+  <Nav />
+  <!-- <router-view /> -->
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css");
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+// text-align: center;
+// color: #2c3e50;
+// }
 
-nav {
-  padding: 30px;
+// nav {
+//   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,96 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <div class="wrapper">
+    <div class="vertical-nav shadow-lg">
+      <h2>Menu</h2>
+      <ul>
+        <li class="nav-link">
+          <RouterLink to=""
+            ><i class="bi bi-house-fill icons"></i> Home</RouterLink
+          >
+        </li>
+        <li class="nav-link">
+          <RouterLink to=""
+            ><i class="bi bi-person icons"></i> Perfil</RouterLink
+          >
+        </li>
+        <li class="nav-link">
+          <RouterLink to=""><i class="bi bi-book icons"></i> Listas</RouterLink>
+        </li>
+        <li class="nav-link">
+          <RouterLink to=""
+            ><i class="bi bi-backpack2 icons"></i> Boletin</RouterLink
+          >
+        </li>
+        <li class="nav-link">
+          <RouterLink to=""><i class="bi bi-bank icons"></i> Pagos</RouterLink>
+        </li>
+        <li class="nav-link">
+          <RouterLink to="/login"
+            ><i class="bi bi-house-fill icons"></i> Iniciar Sesión</RouterLink
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="down-nav">
+      <div class="title-down">
+        <p></p>
+      </div>
+      <div class="p-4"></div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+template {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: black;
+
+  &:hover {
+    color: aquamarine;
+    transform: scale(1.05);
+    transition: 0.2s;
+  }
+}
+.wrapper {
+  display: flex;
+  position: relative;
+  box-sizing: border-box;
+}
+.wrapper .vertical-nav {
+  text-decoration: none;
+  position: fixed;
+  width: 300px;
+  height: 100%;
+  background: linear-gradient(150deg, #f18597, #0097b8);
+  padding: 30px 0;
+}
+.wrapper .vertical-nav h2 {
+  color: aquamarine;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 20px;
+}
+.wrapper .vertical-nav ul .nav-link {
+  list-style: none;
+  padding: 15px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+  border-top: 1px solid rgba(255, 255, 255, 0.05);
+}
+.wrapper .vertical-nav ul li a {
+  display: block;
+}
+.wrapper .vertical-nav ul li a .icons {
+  width: 25px;
+}
+// .wrapper .vertical-nav ul li:hover {
+//   // background: #594f8d;
+// }
+</style>
