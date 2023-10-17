08a15f15c52ef570210bf82f55b5a625a0400706
Maximiliano Insfran <maxiinsfran@gmail.com>
2023-10-17 04:59:10 -0300

    agregando la vista home para cada rol

--- src/utils/count-unpaid-fees.js
+++ src/utils/count-unpaid-fees.js
@@ -7,7 +7,7 @@ const PAYMENT_DAY = new Date(
 );
 
 export const countUnpaidFees = (student) => {
-  if (student.fees.length < 1) return 0;
+  if (!student.fees || student.fees.length < 1) return 0;
   const lastPaidFee = student.fees[student.fees.length - 1];
   const paymentDate = new Date(lastPaidFee.paymentDate);
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,106 +29,114 @@ const alertUnpaidFees = (student) => {
 };
 
 const checkLastPaidFee = async (role) => {
-  const id = localStorage.getItem("user-id");
-  const user = await userService.getByRoleAndId(role, id);
+  //const id = localStorage.getItem("user-id");
+  //const user = await userService.getByRoleAndId(role, id);
 
   if (role === "parent") {
-    user.son = await userService.getByRoleAndId("student", user.son);
-    alertUnpaidFees(user.son);
+    user.value.son = await userService.getByRoleAndId(
+      "student",
+      user.value.son
+    );
+    alertUnpaidFees(user.value.son);
   } else {
-    alertUnpaidFees(user);
+    alertUnpaidFees(user.value);
   }
 };
 </script>
 
 <template>
-  <div>
-    <header>
-      <h1>Bienvenido, {{ user.name }} {{ user.lastName }}</h1>
-    </header>
-  </div>
-  <div v-if="role === 'student'">
-    <p>
-      En el Centro Educativo "Educar para transformar" queremos que nuestros
-      estudiantes tengan la mejor experiencia. Por eso les damos las
-      herramientas necesarias para que puedan hacer un seguimiento total de su
-      rendimiento académico.
-    </p>
-    <img src="../assets/logo.jpg" alt="Logo" />
-    <router-link to="/boletin">Ver Boletín</router-link>
-    <router-link to="/profile">Ver Perfil</router-link>
-  </div>
+  <section class="container-fluid">
+    <h1 class="mt-4">Bienvenido, {{ user.name }} {{ user.lastName }}</h1>
 
-  <div v-else-if="role === 'parent'">
-    <p>
-      En el Centro Educativo "Educar para transformar" queremos que nuestros
-      estudiantes tengan la mejor experiencia. Por eso les damos las
-      herramientas necesarias para que puedan hacer un seguimiento total de su
-      rendimiento académico.
-    </p>
-    <img src="../assets/logo.jpg" alt="Logo" />
-    <router-link to="'/boletin' + user.son.id"
-      >Ver boletin de su hijo</router-link
-    >
-  </div>
+    <div v-if="role === 'student'">
+      <p class="p-3">
+        En el Centro Educativo "Educar para transformar" queremos que nuestros
+        estudiantes tengan la mejor experiencia. Por eso les damos las
+        herramientas necesarias para que puedan hacer un seguimiento total de su
+        rendimiento académico.
+      </p>
+      <img class="logo" src="../assets/logo.jpg" alt="Logo" />
+      <div>
+        <router-link to="/boletin"
+          ><button class="btn btn-primary">Ver Boletín</button></router-link
+        >
+        <router-link to="/perfil-user"
+          ><button class="btn btn-primary">Ver Perfil</button></router-link
+        >
+      </div>
+    </div>
 
-  <div v-else-if="role === 'professor'">
-    <p>
-      En el Centro Educativo "Educar para transformar" queremos que nuestros
-      estudiantes tengan la mejor experiencia. Por eso les damos las
-      herramientas necesarias para que puedan hacer un seguimiento total de su
-      rendimiento académico.
-    </p>
-    <img src="../assets/logo.jpg" alt="Logo" />
-    <router-link to="/list-alumno">Lista de Alumnos</router-link>
-    <router-link to="/notasAlumnos-alumno-id">Ver Notas de Alumnos</router-link>
-  </div>
+    <div v-else-if="role === 'parent'">
+      <p class="p-3">
+        En el Centro Educativo "Educar para transformar" queremos que los padres
+        de nuestros estudiantes tengan la mejor experiencia. Por eso les damos
+        las herramientas necesarias para que puedan hacer un seguimiento total
+        de su rendimiento académico.
+      </p>
+      <img class="logo" src="../assets/logo.jpg" alt="Logo" />
+      <div>
+        <router-link to="'/boletin' + user.son.id"
+          ><button class="btn btn-primary">
+            Ver boletin de su hijo
+          </button></router-link
+        >
+      </div>
+    </div>
 
-  <div v-else-if="role === 'authority'"></div>
+    <div v-else-if="role === 'professor'">
+      <p class="p-3">
+        En el Centro Educativo "Educar para transformar" queremos que nuestros
+        profesores tengan la mejor experiencia. Por eso les damos las
+        herramientas necesarias para que puedan hacer un seguimiento total del
+        rendimiento académico de sus alumnos.
+      </p>
+      <img class="logo" src="../assets/logo.jpg" alt="Logo" />
+      <div>
+        <router-link to="/list-alumno"
+          ><button class="btn btn-primary">
+            Lista de Alumnos
+          </button></router-link
+        >
+      </div>
+    </div>
 
-  <div v-else-if="role === 'personal'">
-    <p>
-      En el Centro Educativo "Educar para transformar" queremos que nuestros
-      estudiantes tengan la mejor experiencia. Por eso les damos las
-      herramientas necesarias para que puedan hacer un seguimiento total de su
-      rendimiento académico.
-    </p>
-    <img src="../assets/logo.jpg" alt="Logo" />
-    <router-link to="/pay-fee"
-      >Registrar pago de cuota de estudiante</router-link
-    >
-  </div>
+    <div v-else-if="role === 'authority'">
+      <p class="p-3">
+        En el Centro Educativo "Educar para transformar" queremos que nuestras
+        autoridades tengan la mejor experiencia. Por eso les damos las
+        herramientas necesarias para que puedan hacer un seguimiento total del
+        centro educativo.
+      </p>
+      <img class="logo" src="../assets/logo.jpg" alt="Logo" />
+    </div>
+
+    <div v-else-if="role === 'personal'">
+      <p class="p-3">
+        En el Centro Educativo "Educar para transformar" queremos que nuestros
+        empleados tengan la mejor experiencia. Por eso les damos las
+        herramientas necesarias para que puedan hacer su trabajo de la mejor
+        forma posible.
+      </p>
+      <img class="logo" src="../assets/logo.jpg" alt="Logo" />
+      <div>
+        <router-link to="/pay-fee"
+          ><button class="btn btn-primary">
+            Registrar pago de cuota de estudiante
+          </button></router-link
+        >
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped="scss">
-.content {
-  text-align: center;
-  margin-top: 20px;
+.logo {
+  height: 360px;
+  width: auto;
+  margin: auto;
 }
 
-.image-container img {
-  display: block;
-  margin: 0 auto;
-  width: 150px;
-}
-
-.router-links {
-  margin: 20px 0;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center;
-}
-
-.router-link {
-  text-decoration: none;
-  padding: 5px 10px;
-  background-color: #007bff;
-  color: white;
-  border-radius: 5px;
-}
-
-.router-link:hover {
-  background-color: #0056b3;
+.btn {
+  margin: 25px 15px 15px 15px;
 }
 </style>
